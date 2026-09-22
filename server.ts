352b4fa194049dc89e67f991439ba6be286be2c0
rd227344-boop <rd227344@gmail.com>
2026-09-22 09:39:56 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { createServer as createViteServer } from 'vite';
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
